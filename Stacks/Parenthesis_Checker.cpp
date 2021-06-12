ca0fff8e941971441e147c20ca025f6977688cfe
ANISHA SINGH <anishasingh439@gmail.com>
2021-06-12 21:33:23 +0530

    Create Parenthesis_Checker.cpp

---
+++ Stacks/Parenthesis_Checker.cpp
@@ -0,0 +1,81 @@
+ // Anisha Singh
+ // EEE
+ // BV'23
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool check_parenthesis(string str) 			 //Boolean Function to check the given expression 
+{
+	stack <int> s;
+
+	for(int i=0; i<str.length(); i++)
+	{
+		if(str[i]=='(' || str[i]=='[' || str[i]=='{' )
+		{
+			s.push(str[i]);    					//Push the element into stack
+		}
+
+		else
+		{
+			switch(str[i])
+			{
+				case ')':
+				 if(s.empty() || s.top()!='(')
+				 {
+				 	return false;
+				 }
+				 s.pop();
+				 break;
+
+				case ']':
+				  if(s.empty() || s.top()!='[')
+				  {
+				  	return false;
+				  }
+				  s.pop();
+				  break;
+
+				case '}':
+				 if(s.empty() || s.top()!='}')
+				 {
+				 	return false;
+				 }
+				 s.pop();
+				 break;
+			}
+		}
+	}
+
+	return s.empty();
+
+}
+
+
+int main()
+{
+	string exp;
+	cin>>exp;
+
+	if (check_parenthesis(exp))   			 //Calling the function
+      cout << "true";
+   else
+      cout << "false";
+  
+  return 0;
+}
+
+
+   // Input : [(a+b)]
+   // Output:  True
+
+   // Input : [)]
+   // Output:  False
+ 
+ 	// Time Complexity : O(n)
+ 	// Space Complexity : O(n)
+	
+
+
