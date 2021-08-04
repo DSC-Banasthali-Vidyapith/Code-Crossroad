58129a8ae22874f5dc8afe2f4590646044a515de
neilotpal <58064847+neilotpal@users.noreply.github.com>
2021-08-04 10:50:04 +0530

    Update Largest_sum_subarray.cpp

--- Array/Largest_sum_subarray.cpp
+++ Array/Largest_sum_subarray.cpp
@@ -1,4 +1,6 @@
 /* Given an array of positive and negative integers. We have to find subarray having maximum sum */
+/*The simple idea of Kadane’s algorithm is to look for all positive contiguous segments of the array (max_ending_here is used for this). And keep track of maximum sum contiguous segment among all positive segments (max_so_far is used for this). Each time we get a positive-sum compare it with max_so_far and update max_so_far if it is greater than max_so_far */
+
 #include<iostream>
 #include<limits.h>
 using namespace std;
