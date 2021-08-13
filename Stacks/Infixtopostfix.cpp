a8169b10c797da0ef45bc7260a1297b4c8fdb0f7
Keya00 <keya.khandelwal@gmail.com>
2021-08-14 00:23:26 +0530

    Added code to change an expression from infix to postfix

---
+++ Stacks/Infixtopostfix.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int precedence(char sym)
+{
+    if(sym == '^') return 4;
+    if(sym == '*' || sym == '/') return 3;
+    if(sym == '+' || sym == '-') return 2;
+    if(sym == '(') return 1;
+}
+
+int main() {
+    int t;
+	cin >> t;
+	while(t--) {
+	    stack<char> stk;
+	    string s, res="";
+	    cin >> s;
+	    for(int i=0; i<s.length(); i++) {
+	        if(s[i]>=65 && s[i]<=90) {
+	            res += s[i];
+	        }
+	        else if(stk.empty() && (s[i]=='*' || s[i]=='/' || s[i]=='+' || s[i]=='-' || s[i]=='^')) {
+	            stk.push(s[i]);
+	        }
+	        else if(s[i]=='(') {
+	            stk.push(s[i]);
+	        }
+	        else if(s[i]==')') {
+	            while(stk.top()!='(') {
+	                res += stk.top();
+	                stk.pop();
+	            }
+	            stk.pop();
+	        }
+	        else if(!stk.empty() && precedence(stk.top())>=precedence(s[i])) {
+	            while(!stk.empty() && precedence(stk.top())>=precedence(s[i]) && stk.top()!='(')
+                {
+                    res += stk.top();
+                    stk.pop();
+                }
+                stk.push(s[i]);
+	        }
+	        else if(!stk.empty() && precedence(s[i])>precedence(stk.top())) {
+                stk.push(s[i]);
+	        }
+	    }
+	    while(!stk.empty()) {
+	        res += stk.top();
+	        stk.pop();
+	    }
+	    cout << res << "\n";
+	}
+	return 0;
+}
