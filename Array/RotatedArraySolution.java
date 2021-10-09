792de191498e4b1fc57c29bc899d78f0b6fc929b
Khushipm <78797034+Khushipm@users.noreply.github.com>
2021-10-09 20:09:41 +0530

    Update RotatedArraySolution.java

--- Array/RotatedArraySolution.java
+++ Array/RotatedArraySolution.java
@@ -1,40 +1,33 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class RotateArray {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter length of array: ");
-        int len = in.nextInt();
-        System.out.print("Enter numbers: ");
-        int[] nums = new int[len];
-        for (int i=0; i < len; i++){
-            nums[i] = in.nextInt();
-        }
-        System.out.print("Enter number of rotations: ");
-        int k = in.nextInt();
-        System.out.println("Input Array: "+ Arrays.toString(nums));
-        rotate(nums, k);
+class Solution {
+    public void rotate(int[] nums, int k)
+    {
+       //if k > length of array for that case
+    k = k % nums.length;
+    
+    //reverse 1st part
+    reverse(nums, 0, nums.length-k-1);
+    
+    //reverse 2nd part
+    reverse(nums, nums.length-k, nums.length-1);
+    
+    //now reverse the full arrary
+    reverse(nums, 0, nums.length-1);
+        
+        
     }
-    public static void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n;
-        rotateSubArray(nums, 0, n-k);
-        rotateSubArray(nums, n-k, n);
-        rotateSubArray(nums, 0 , n);
-        System.out.println("Rotated Array: "+ Arrays.toString(nums));
-    }
-    public static void rotateSubArray(int[] nums, int start, int end){
-        while(start < end){
-            swap(nums, start, end-1);
-            start++;
-            end--;
-        }
-    }
-    public static void swap(int[] nums, int i, int j){
-        int temp = nums[i];
+    
+     public static void reverse(int[] nums, int s, int e){
+    
+    int i = s;
+    int j = e;
+    
+    while(i < j){
+        int t = nums[i];
         nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
+        nums[j] = t;
+        
+        i++;
+        j--;
+    } 
+}
 }
