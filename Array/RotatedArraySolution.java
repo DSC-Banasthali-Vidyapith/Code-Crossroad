2d8e53b474660080c85c1ff395a97a11ee8b9e5b
Khushipm <78797034+Khushipm@users.noreply.github.com>
2021-10-09 23:03:27 +0530

    Update RotatedArraySolution.java

--- Array/RotatedArraySolution.java
+++ Array/RotatedArraySolution.java
@@ -1,3 +1,4 @@
+/*Given an array nums[], rotate the array to the right by k steps, where k is non-negative.*/
 class Solution {
     public void rotate(int[] nums, int k)
     {
@@ -31,3 +32,23 @@ class Solution {
     } 
 }
 }
+/*
+TEST CASE
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+
+Constraints:
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+
+*/
+
+
+
+
